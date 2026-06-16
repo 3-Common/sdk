b1a14c97d4b29878d8548eb2b78397dabbebf8b8
Aidan Garvey <aidangarvey@3common.com>
2026-06-16 15:13:44 -0500

    fix(properties): handle forms methods in dispatch runner

--- sdk-node/test/conformance/dispatch-properties.ts
+++ sdk-node/test/conformance/dispatch-properties.ts
@@ -54,6 +54,15 @@ export function dispatchProperties(
     case 'archive':
     case 'unarchive':
     case 'resolve':
+    case 'duplicate':
+    case 'addElement':
+    case 'updateElement':
+    case 'deleteElement':
+    case 'moveElement':
+    case 'enableOtherOption':
+    case 'disableOtherOption':
+    case 'addLogicRule':
+    case 'removeLogicRule':
       throw new Error(`properties: unsupported method '${call.method}'`)
   }
 }
